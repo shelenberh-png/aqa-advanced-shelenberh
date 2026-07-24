c3150386fbc46471373535cecf8a3b172f71f72a
Yevhenii Shelenberh <shelenberh@gmail.com>
2026-07-24 15:16:33 +0300

    lesson 5 commit from shelenberh

---
+++ lesson-5-JS/task3.js
@@ -0,0 +1,13 @@
+// task 3 through 'for' loop
+const number = 5;
+for (let i = 1; i <= 10; i++) {
+  const result = number * i;
+  console.log(`${number} x ${i} = ${result}`);
+}
+// task 3 through 'while' loop
+let i = 1;
+while (i <= 10) {
+  const result = number * i;
+  console.log(`${number} x ${i} = ${result}`);
+  i++;
+}
